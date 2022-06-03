65be77831578244fa426774efe131bec9b663283
Diego Abad <dsarfirths@gmail.com>
2022-06-03 15:40:29 -0500

    commit 2

--- Index.py
+++ Index.py
@@ -10,6 +10,10 @@ def layout():
 def servicios():
     return render_template("servicios.html")
 
+@app.route('/videojuegos')
+def videojuegos():
+    return render_template("videojuegos.html")
+
 #La última pieza sirve para que el servidor actualice los cambios sin volver a desplegar el server de nuevo
 if __name__ == '__main__':
     app.run(debug=True)
